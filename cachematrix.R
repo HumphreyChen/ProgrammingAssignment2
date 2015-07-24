e4ada834c3ea9d1d7dc9ebbd0a22b74d1bee1f9a
HumphreyChen <hsinlovehsin@gmail.com>
2015-07-24 11:25:30 -0500

    finished makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-Programming (Assignment #2): cache solver of matrix inverse
 
-## Write a short comment describing this function
-
+##----------------------------------------------------------------
+## A function for matrix assignment: Return a list of functions
+## Set and get the original/inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(im) m <<- im
+    getInverse <- function() m
+    
+    ## return the list of functions
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
